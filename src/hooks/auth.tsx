97a120a212fe6e0a400b6efed4e74a88f2dfb9c3
Angel Fernandes <angelfernandes07@hotmail.com>
2021-03-13 14:46:26 -0300

    private routes

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -14,6 +14,7 @@ interface SignInCredencials {
 }
 interface AuthContextData {
   user: object;
+  loading: boolean;
   signIn(credentials: SignInCredencials): Promise<void>;
   signOut(): void;
 }
@@ -27,6 +28,7 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData);
 
 export const AuthProvider: React.FC = ({ children }) => {
   const [data, setData] = useState<AuthState>({} as AuthState);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadStorageData(): Promise<void> {
@@ -38,6 +40,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       if (token[1] && user[1]) {
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
+
+      setLoading(false);
     }
     loadStorageData();
   }, []);
@@ -65,7 +69,7 @@ export const AuthProvider: React.FC = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ user: data.user, signIn, signOut }}>
+    <AuthContext.Provider value={{ user: data.user, loading, signIn, signOut }}>
       {children}
     </AuthContext.Provider>
   );
